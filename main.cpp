cc2d02d81ac1eebc51030227713df2066dc9eaf3
sodipe <98396909+so-dipe@users.noreply.github.com>
2024-11-26 15:05:06 +0000

    Initial Files created

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string name;
+    cout << "Hello There! \nWhat's your name? ";
+    cin >> name;
+    cout << "Welcome, " << name << "! Hope you have a great time here \n";
+    return 0;
+}
